8204d13f36dda95ccc80e2e6b80198b87900733a
kaikw <kaikw@students.cs.ubc.ca>
2023-11-19 12:23:01 -0800

    Added the base for hip and wrist

--- src/stretches/hip.js
+++ src/stretches/hip.js
@@ -1,9 +1,55 @@
-const Hip = () => {
-    return (
-      <div>
-        <p>Hip</p>
+
+import React from 'react';
+import "../App.css";
+import logo from "../logo.svg";
+
+
+const hip = () => {
+  return (
+    <>
+    <div>
+      <header className="exercise-header">
+        <title>This is an exercise</title>
+      </header>
+
+      <div className="row">
+        <div className="box-container">
+          <div>
+            <img src={logo} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+
+        <div className="box-description">
+          <img src={logo} alt="Logo" />
+          <p>Placeholder Image</p>
+        </div>
       </div>
-    );
-  };
-  
-  export default Hip;
+    </div>
+    
+
+    <div>
+      <header className="exercise-header">
+        <title>This is an exercise</title>
+      </header>
+
+      <div className="row">
+        <div className="box-container">
+          <div>
+            <img src={logo} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+
+        <div className="box-description">
+          <img src={logo} alt="Logo" />
+          <p>Placeholder Image</p>
+        </div>
+      </div>
+    </div>
+    </>
+
+  );
+};
+
+export default hip;
--- src/stretches/wrist.js
+++ src/stretches/wrist.js
@@ -1,9 +1,55 @@
-const Wrist = () => {
-    return (
-      <div>
-        <p>Wrist</p>
+
+import React from 'react';
+import "../App.css";
+import logo from "../logo.svg";
+
+
+const wrist = () => {
+  return (
+    <>
+    <div>
+      <header className="exercise-header">
+        <title>This is an exercise</title>
+      </header>
+
+      <div className="row">
+        <div className="box-container">
+          <div>
+            <img src={logo} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+
+        <div className="box-description">
+          <img src={logo} alt="Logo" />
+          <p>Placeholder Image</p>
+        </div>
       </div>
-    );
-  };
-  
-  export default Wrist;
+    </div>
+    
+
+    <div>
+      <header className="exercise-header">
+        <title>This is an exercise</title>
+      </header>
+
+      <div className="row">
+        <div className="box-container">
+          <div>
+            <img src={logo} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+
+        <div className="box-description">
+          <img src={logo} alt="Logo" />
+          <p>Placeholder Image</p>
+        </div>
+      </div>
+    </div>
+    </>
+
+  );
+};
+
+export default wrist;
