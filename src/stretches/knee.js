4c8fe6df762938fd8115e0b00ebcb1972028ea0b
Brandon Chen <brandon.zchen6@gmail.com>
2023-11-19 14:11:57 -0800

    added knee gifs

--- src/stretches/knee.js
+++ src/stretches/knee.js
@@ -2,6 +2,11 @@
 import React from 'react';
 import "../App.css";
 import gif1 from "./kneestretches/CalfRaises.webp";
+import gif2 from "./kneestretches/HalfSquat.webp";
+import gif3 from "./kneestretches/HipAbduction.webp";
+import gif4 from "./kneestretches/StandingCalfStretch.webp";
+import gif5 from "./kneestretches/StraightLegRaise.webp";
+import gif6 from "./kneestretches/SupineHamstringStretch.webp";
 import headerlogo from "../stretch-logo.png";
 import Header from '../components/header';
 
@@ -33,13 +38,93 @@ const knee = () => {
       <div>
         <header className="exercise-header">
           
-          <title>This is an exercise</title>
+          <title>Half Squat</title>
         </header>
   
         <div className="row">
           <div className="box-container">
             <div>
-              <img src={gif1} alt="Logo" />
+              <img src={gif2} alt="Logo" />
+              <p>Placeholder Image</p>
+            </div>
+          </div>
+  
+          <div className="box-description">
+            <img src={gif1} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <header className="exercise-header">
+          
+          <title>Hip Abduction</title>
+        </header>
+  
+        <div className="row">
+          <div className="box-container">
+            <div>
+              <img src={gif3} alt="Logo" />
+              <p>Placeholder Image</p>
+            </div>
+          </div>
+  
+          <div className="box-description">
+            <img src={gif1} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <header className="exercise-header">
+          
+          <title>Standing Calf Stretch</title>
+        </header>
+  
+        <div className="row">
+          <div className="box-container">
+            <div>
+              <img src={gif4} alt="Logo" />
+              <p>Placeholder Image</p>
+            </div>
+          </div>
+  
+          <div className="box-description">
+            <img src={gif1} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <header className="exercise-header">
+          
+          <title>Straight Leg Raise</title>
+        </header>
+  
+        <div className="row">
+          <div className="box-container">
+            <div>
+              <img src={gif5} alt="Logo" />
+              <p>Placeholder Image</p>
+            </div>
+          </div>
+  
+          <div className="box-description">
+            <img src={gif1} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <header className="exercise-header">
+          
+          <title>Supine Hamstring Stretch</title>
+        </header>
+  
+        <div className="row">
+          <div className="box-container">
+            <div>
+              <img src={gif6} alt="Logo" />
               <p>Placeholder Image</p>
             </div>
           </div>
