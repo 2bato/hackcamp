65a5373e6a6f394a763e8cf1c44218b315817c23
2bato <andrewfan030114@gmail.com>
2023-11-19 10:56:30 -0800

    Setup bodymap of anatomy and buttons

---
+++ src/bodymap.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./bodymap.css";
+import anatomy from "./bodymap.jpg";
+
+const Bodymap = () => {
+  return (
+    <div className="bodymap">
+      <img src={anatomy} className="anatomy" />
+
+      <button className="neck"></button>
+      <button className="elbow"></button>
+      <button className="shoulder"></button>
+      <button className="knee"></button>
+      <button className="wrist"></button>
+    </div>
+  );
+};
+
+export default Bodymap;
