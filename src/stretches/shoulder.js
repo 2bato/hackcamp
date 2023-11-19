75f320c2f9c8a29801bcf3356483ca8ccb1005e8
kaikw <kaikw@students.cs.ubc.ca>
2023-11-19 11:54:07 -0800

    Added template created for all body parts

---
+++ src/stretches/shoulder.js
@@ -0,0 +1,55 @@
+
+import React from 'react';
+import "../App.css";
+import logo from "../logo.svg";
+
+
+const shoulder = () => {
+  return (
+    <>
+    <div>
+      <header className="exercise-header">
+        <title>This is an exercise</title>
+      </header>
+
+      <div className="row">
+        <div className="box-container">
+          <div>
+            <img src={logo} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+
+        <div className="box-description">
+          <img src={logo} alt="Logo" />
+          <p>Placeholder Image</p>
+        </div>
+      </div>
+    </div>
+    
+
+    <div>
+      <header className="exercise-header">
+        <title>This is an exercise</title>
+      </header>
+
+      <div className="row">
+        <div className="box-container">
+          <div>
+            <img src={logo} alt="Logo" />
+            <p>Placeholder Image</p>
+          </div>
+        </div>
+
+        <div className="box-description">
+          <img src={logo} alt="Logo" />
+          <p>Placeholder Image</p>
+        </div>
+      </div>
+    </div>
+    </>
+
+  );
+};
+
+export default shoulder;
