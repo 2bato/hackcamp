badacf82320532808c7038a1ded3fb0edfcd7337
2bato <andrewfan030114@gmail.com>
2023-11-19 11:20:46 -0800

    Added route from elbow button

--- src/bodymap.js
+++ src/bodymap.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./bodymap.css";
 import anatomy from "./bodymap.jpg";
-
+import { Link } from "react-router-dom";
 const Bodymap = () => {
   return (
     <div className="bodymap">
       <img src={anatomy} className="anatomy" />
-
+    
       <button className="neck"></button>
+      <Link to="/elbow">
       <button className="elbow"></button>
+      </Link>
       <button className="shoulder"></button>
       <button className="knee"></button>
       <button className="wrist"></button>     
