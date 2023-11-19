3882d2c21a2e218606f2486c61ff1658a52bac52
2bato <andrewfan030114@gmail.com>
2023-11-19 09:57:01 -0800

    Created landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { logDOM } from "@testing-library/react";
+import "./App.css";
+import bodymap from "./bodymap.jpg";
+import logo from "./stretch-logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" />
+        <title>JointPal</title>
       </header>
+      <img src={bodymap} className="App-bodymap" />
     </div>
   );
 }
